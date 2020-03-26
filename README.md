2c96575ac5c536528fd5929a39e0dd6ef878e907
Eva Jason <eva.rose.jason@gmail.com>
2020-03-26 09:04:23 -0700

    fixed program to handle non numerical answers and hidden files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Guess How Many Files
-Wed Mar 25 14:56:17 PDT 2020
+Thu Mar 26 09:03:37 PDT 2020
 <br>Number of lines: 
-26
+31
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
-
-files=$(ls -1 | wc -l)
+files=$(ls -1a | wc -l)
 echo "How many files do you think are in this directory?"
+
 function ask {
 	read guess
+	if [[ ! $guess =~ ^[0-9]+$ ]]
+	then 
+		echo "Please enter a number"
+		ask
+	fi
 }
 
-ask
-
-while [[ $guess -ne $files ]]
-do
-	if [[ $guess -lt $files ]]
-	then
-		echo "Your guess was too low, try again."
-	else
-		echo "Your guess was too high, try again."
-	fi
+function try {
 	ask
-done
-
-echo "Congratulations! You got it! There are $files files in this directory"
-
-
-
-
+	while [[ $guess -ne $files ]]
+		do	
+		if [[ $guess -lt $files ]]
+		then
+			echo "Your guess was too low, try again."
+		elif [[ $guess -gt $files ]]
+		then
+			echo "Your guess was too high, try again."
+		else
+			echo "Try again"
+		fi
+		ask
+		done
+		echo "Congratulations! You got it! There are $files files in this directory"
+}
+try
